34a6a81de07e39182d21f83d2f82d91ff7ba62f0
Aleksey Logachev <a.logachev@crpo.su>
2025-08-26 21:40:21 +0300

    hw4 Added a default config service realization

---
+++ src/Config/ConfigService.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Alogachev\Homework\Config;
+
+class ConfigService
+{
+    private const string BASE_CONFIG_PATH = __DIR__ . '/../../config';
+
+    public function get(string $configFilePath): array
+    {
+        $resultPath = self::BASE_CONFIG_PATH . '/' . $configFilePath . '.php';
+        if (!file_exists($resultPath)) {
+            return [];
+        }
+
+        return [];
+    }
+}
